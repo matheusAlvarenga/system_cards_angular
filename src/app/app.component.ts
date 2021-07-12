a0619cc520f5511de13e528f6cdef7508b7839d6
Matheus Alvarenga <matheus@hubway.com.br>
2021-07-12 19:20:09 -0300

    Finished system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+    posts = [
+    {
+        title: "Neat Tree",
+        username: "nature",
+        content: "Saw this awesome tree during my hike today.",
+        imageURL: "assets/tree.jpeg"
+    },
+    {
+        title: "Snowy Mountain",
+        username: "mountainlover",
+        content: "Here's a picture of a snowy mountain",
+        imageURL: "assets/mountain.jpeg"
+    },
+    {
+        title: "Mountain Biking",
+        username: "biking12222",
+        content: "I did some biking today",
+        imageURL: "assets/biking.jpeg"
+    },
+]
 }
